1d60744bf3bc72386a211ab0dc3d2ccfb7fc2cd3
FlutLab.io <>
2022-01-10 13:38:20 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: InteractiveViewer(
-          child: SingleChildLayoutDelegate(
+          child: Container(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
